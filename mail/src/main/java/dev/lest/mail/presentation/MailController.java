c4b5ae49be6f38adb893bd1d6bb042b0a217159c
Leonardo Figorelli Santana <leonardo.santana@razzo.razzo>
2025-12-26 12:54:40 -0300

    Feat: Constroi a base do CRUD de mail

--- mail/src/main/java/dev/lest/mail/presentation/MailController.java
+++ mail/src/main/java/dev/lest/mail/presentation/MailController.java
@@ -1,11 +1,27 @@
 package dev.lest.mail.presentation;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/mail/")
 public class MailController {
+    @PostMapping()
+    Object createMailEndPoint(Object createMailRequest) {
+        return new Object();
+    }
 
+    @GetMapping()
+    Object findMailEndPoint(Object mailRequest) {
+        return new Object();
+    }
 
+    @PatchMapping()
+    Object updateMailEndPoint(Object updateMailRequest) {
+        return new Object();
+    }
+
+    @DeleteMapping()
+    Object deleteMailEndPoint(Object deleteMailRequest) {
+        return new Object();
+    }
 }
