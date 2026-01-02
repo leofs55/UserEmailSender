a506bc61029be34e24385cc512f0f55ff5d3e1ff
Leonardo <leofigorelli@gmail.com>
2026-01-02 18:35:51 -0300

    feat: Cria algumas coisas migration.

--- mail/src/main/resources/db/migration/V1__CREATE_TABLE_TB_MAIL.sql
+++ mail/src/main/resources/db/migration/V1__CREATE_TABLE_TB_MAIL.sql
@@ -1,10 +1,12 @@
+
+
 CREATE TABLE TB_MAIL(
-    email_id,
-    user_id,
-    email_from,
-    email_to,
-    email_subject,
-    body,
-    send_date_email,
+    email_id UUID PRIMARY KEY,
+    user_id UUID,
+    email_from VARCHAR(255) NOT NULL,
+    email_to VARCHAR(255) NOT NULL,
+    email_subject VARCHAR(255) NOT NULL,
+    body TEXT,
+    send_date_email ,
     email_status
 );
