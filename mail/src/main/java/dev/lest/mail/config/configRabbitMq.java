43ff16afe11935f6cb374752bc9960dfc8f61626
Leonardo Figorelli Santana <leofigorelli@gmail.com>
2026-01-06 21:20:40 -0300

    descobrir o pq o consumer náo consegue traduzir a message

---
+++ mail/src/main/java/dev/lest/mail/config/configRabbitMq.java
@@ -0,0 +1,43 @@
+package dev.lest.mail.config;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rabbitmq.client.ConnectionFactory;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class configRabbitMq {
+
+    private final String queueName = "mail-queue";
+
+    @Bean
+    public Queue queue() {
+        return new Queue(queueName, true);
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(
+            ConnectionFactory connectionFactory,
+            Jackson2JsonMessageConverter jsonMessageConverter) {
+
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setMessageConverter(jsonMessageConverter);
+
+        // Use DefaultAmqpHeaderMapper which is generally more stable than SimpleAmqpHeaderMapper
+        DefaultA headerMapper = DefaultAmqpHeaderMapper.inboundMapper();
+        // You can also tell it to ignore certain headers if they continue to cause issues
+        // headerMapper.setInboundHeaderNames("!priority", "*");
+
+        return factory;
+    }
+}
---
+++ mail/src/main/java/dev/lest/mail/consumer/MailConsumer.java
@@ -0,0 +1,15 @@
+package dev.lest.mail.consumer;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MailConsumer {
+
+    @RabbitListener(queues = "mail-queue")
+    public void listenMailQueue(@Payload String string) {
+        System.out.println(string);
+    }
+
+}
