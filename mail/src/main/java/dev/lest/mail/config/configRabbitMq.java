a5764d5bbcc9daeb5fea6bc3b19de1bea73f8b27
Leonardo Figorelli Santana <leofigorelli@gmail.com>
2026-01-07 21:49:57 -0300

    resolvido

--- mail/src/main/java/dev/lest/mail/config/configRabbitMq.java
+++ mail/src/main/java/dev/lest/mail/config/configRabbitMq.java
@@ -2,10 +2,8 @@ package dev.lest.mail.config;
 
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.rabbitmq.client.ConnectionFactory;
 import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -19,25 +17,9 @@ public class configRabbitMq {
         return new Queue(queueName, true);
     }
 
-//    @Bean
-//    public Jackson2JsonMessageConverter jsonMessageConverter() {
-//        return new Jackson2JsonMessageConverter();
-//    }
-//
-//    @Bean
-//    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(
-//            ConnectionFactory connectionFactory,
-//            Jackson2JsonMessageConverter jsonMessageConverter) {
-//
-//        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
-//        factory.setConnectionFactory(connectionFactory);
-//        factory.setMessageConverter(jsonMessageConverter);
-//
-//        // Use DefaultAmqpHeaderMapper which is generally more stable than SimpleAmqpHeaderMapper
-//        DefaultAmqpHeaderMapper headerMapper = DefaultAmqpHeaderMapper.inboundMapper();
-//        // You can also tell it to ignore certain headers if they continue to cause issues
-//        // headerMapper.setInboundHeaderNames("!priority", "*");
-//
-//        return factory;
-//    }
+    @Bean
+    public JacksonJsonMessageConverter messageConverter() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return new JacksonJsonMessageConverter(String.valueOf(objectMapper));
+    }
 }
